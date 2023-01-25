161fd2ef03377ed48bc066f74c93e599db0c8a58
Austin James <austinwood360@gmail.com>
2023-01-24 20:59:55 -0700

    clearer variable names in ote_minimizer

--- ote_minimizer.py
+++ ote_minimizer.py
@@ -6,23 +6,28 @@ from itertools import permutations, combinations_with_replacement, chain
     # are defined by the value in the array
     # ex. [1, 3, 1] returns [[0], [1, 2, 3], [4]]
 def slicer(array):
-    n = list(range(sum(array)))
-    r = []
-    j = 0
-    for i in array:
-        r.append(n[j:j+i])
-        j += i
-    return r
+    n_subarrays = list(range(sum(array)))
+    return_array = []
+    subarray_start_value = 0
+    for subarray_length in array:
+        return_array.append(
+            n_subarrays[
+                subarray_start_value:subarray_start_value+subarray_length
+            ]
+        )
+        subarray_start_value += subarray_length
+    return return_array
 
 
 # returns loader assignment which minimizes OTEs
 def minimize_ote(loader_dict, door_array):
+
     # calculate OTEs for one loader (in assignment)
     def calc_ote(loader, assignment):
         # calculate loader pph
         pph = loader_dict[loader] * efficiency[len(assignment)]
         # sum doors fph
-        fph = sum([door_array[i] for i in assignment])
+        fph = sum([door_array[door] for door in assignment])
         # subtract doors fph from loader pph
         sub = pph - fph
 
@@ -33,10 +38,8 @@ def minimize_ote(loader_dict, door_array):
         pd_otes = 0.0
         loader_otes = []
         for i in range(len(loader_perm)):
-            # get loader name
-            loader = loader_perm[i]
             # calculate loader OTE for assignment
-            load_ote = calc_ote(loader, door_perm[i])
+            load_ote = calc_ote(loader_perm[i], door_perm[i])
             # add individual loader OTE to loader OTE list
             loader_otes.append(load_ote)
             # add individual OTE to PD OTE if negative
@@ -53,17 +56,17 @@ def minimize_ote(loader_dict, door_array):
     }
 
     # for permutations
-    k = len(loader_dict)
-    n = len(door_array)
+    n_loaders = len(loader_dict)
+    n_doors = len(door_array)
     # get sizes of sub-arrays for door permutations
     perm_array = list(chain(
         *[list(
-            set(permutations(comb))) 
-            for comb in 
+            set(permutations(combination))
+            ) for combination in 
                 [
-                    c for c
-                    in combinations_with_replacement(range(1, 5), k)
-                    if sum(c) == n
+                    comb for comb
+                    in combinations_with_replacement(range(1, 5), n_loaders)
+                    if sum(comb) == n_doors
                 ]
         ]
     ))
@@ -82,13 +85,16 @@ def minimize_ote(loader_dict, door_array):
     ind_loader_otes = list()
 
     # iterate through all possible combinations of loaders and door assignments
-    for door_p in door_permutations:
-        for loader_p in loader_permutations:
+    for door_perm in door_permutations:
+        for loader_perm in loader_permutations:
             # calculate PD OTE for loader/assignment combination
-            pd_ote, loader_otes = calc_pd_ote(loader_p, door_p)
+            pd_ote, loader_otes = calc_pd_ote(loader_perm, door_perm)
             # assign loader dispatch and minimized OTE
             if pd_ote > min_ote:
-                loader_dispatch = {loader_p[i] : door_p[i] for i in range(len(loader_p))}
+                loader_dispatch = {
+                    loader_perm[i] : door_perm[i]
+                    for i in range(len(loader_perm))
+                }
                 min_ote = pd_ote
                 ind_loader_otes = loader_otes
 
